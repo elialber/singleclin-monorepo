f36370afe37c789c063b2fc9d36c02f161e168ea
Elialber Lopes <elialberlopes@gmail.com>
2025-07-30 15:25:58 -0300

    refactor: Atualiza lógica de carregamento e tratamento de erros
    - Modifica a função de carregamento no BaseController para usar um setter, melhorando a clareza do código.
    - Adiciona tratamento específico para diferentes tipos de exceções no método _mapExceptionToFailure, aprimorando a robustez do sistema.
    - Remove importação desnecessária no clinic_scanner_screen.dart, otimizando a organização do código.
    - Atualiza comentários no patient_data_bottom_sheet.dart para refletir a intenção futura de implementação de API.
    
    Essas alterações visam melhorar a legibilidade e a eficiência do código, além de garantir um tratamento de erros mais eficaz.

--- packages/mobile/lib/presentation/screens/clinic/clinic_scanner_screen.dart
+++ packages/mobile/lib/presentation/screens/clinic/clinic_scanner_screen.dart
@@ -4,7 +4,6 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-
 import 'package:mobile/core/routes/app_routes.dart';
 import 'package:mobile/presentation/widgets/manual_code_dialog.dart';
 import 'package:mobile/presentation/widgets/patient_data_bottom_sheet.dart';
